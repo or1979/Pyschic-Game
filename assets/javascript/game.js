5b2b35d2aee409d995483e824f583190fbf632f6
or1979 <orobinson79@gmail.com>
2019-03-19 11:14:10 -0400

    firstcommit

---
+++ assets/javascript/game.js
@@ -0,0 +1,25 @@
+var wins = 0; // player starts off with 0 wins
+var losses = 0; // player starts off with 0 losses
+var guessesleft = 10; //player has 10 trys to guess a letter, each miss deducts a guess
+var userguesses = []; // keeps track of users guess
+var comgenguess = []; // computers guess
+
+
+
+
+
+
+
+var alpha = [ "A", "B","C", "D","E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"];
+
+
+for (var i = 0; i < alpha.length; i++) {
+
+    math.floor(math.random() * alpha.length) .toLowerCase();
+    
+
+}
+
+if alpha.length === { }
+
+ 
